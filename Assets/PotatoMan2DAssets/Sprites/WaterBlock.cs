42b37518d40f446ae7277bd1b17a9de48ba059e0
Scott Bromander <scott@primeacademy.io>
2016-06-30 21:04:30 -0500

    initial commit

---
+++ Assets/PotatoMan2DAssets/Sprites/WaterBlock.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterBlock : MonoBehaviour {
+
+	private Animator animationController;
+
+
+	// Use this for initialization
+	void Start () {
+		animationController = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D hit){
+		if (hit.CompareTag ("Player")) {
+			animationController.SetTrigger ("Fall");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
